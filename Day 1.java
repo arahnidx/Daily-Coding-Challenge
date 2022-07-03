09b26b72c90e4758a16130816ae10a06224c7208
arahnidx <sidneymizael@ymail.com>
2022-07-03 15:33:06 -0300

    Refractoring

--- Day 1.java
+++ Day 1.java
@@ -7,7 +7,6 @@ public class Main {
 		For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 		Bonus: Can you do this in one pass?*/
 
-
 	public static boolean method(int[] N, int k) {
 		for (int a : N){
 			for (int b : N){
@@ -19,13 +18,10 @@ public class Main {
 		return false;
 	}
 
-
-
 	public static void main(String[] args) {
 		int[] N = new int[]{ 10, 15, 3, 7 };
 		int k = 10;
 
 		System.out.println(method(N, k));
-
 	}
 }
